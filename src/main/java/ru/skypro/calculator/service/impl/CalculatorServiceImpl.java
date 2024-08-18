2474048f8e2dd634428187f0795a8d1b9ef7e405
iRUNCHIKK <btkv_irina@icloud.com>
2024-08-18 21:56:41 +0500

    Completed tasks

---
+++ src/main/java/ru/skypro/calculator/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,28 @@
+package ru.skypro.calculator.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.calculator.service.CalculatorService;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    @Override
+    public Integer plus(Integer num1, Integer num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public Integer minus(Integer num1, Integer num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public Integer multiply(Integer num1, Integer num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public Integer divide(Integer num1, Integer num2) {
+        return num1 / num2;
+    }
+}
